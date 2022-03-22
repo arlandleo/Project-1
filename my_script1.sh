1f99c244a69127f8bd8f048238f946cbee71ce0f
arlandleo <arleon663@gmail.com>
2022-03-21 22:47:36 -0400

    S3 bucket creation

--- my_script1.sh
+++ my_script1.sh
@@ -4,12 +4,21 @@
 
 echo " Type three conditions to create s3 bucket "
 
-read -p 'first_name: ' arland
-read -p 'second_name: ' leo
-read -p 'third_name: ' arlandleo
+read -p 'first_name: ' name1
+read -p 'second_name: ' name2
+read -p 'third_name: ' name3
 
-if (( $first_name == "arland"  &&  $second_name == "leo"  &&  $third_name == "arlandleo"  ))
-then 
-    echo "aws s3api create-bucket --bucket 1name2name3name --region us-east-1"
 
+if ! grep aws_access_key_id ~/.aws/credentials; then
+   if ! grep aws_access_key_id ~/.aws/credentials; then
+   echo "install was cli"
+   exit 1
+   fi
 fi
+aws s3api create-bucket --bucket $name1-$name2-$name3 --region us-east-1
+
+# if (( $first_name == "arland"  &&  $second_name == "leo"  &&  $third_name == "arlandleo"  ))
+# then 
+#     echo "aws s3api create-bucket --arlandleoarlandleo --region us-east-1"
+
+# fi
